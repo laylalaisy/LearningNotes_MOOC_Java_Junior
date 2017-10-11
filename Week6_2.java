f282e84d37a1f74e61910d9276c6640b9df38159
Layla Laisy <layla.laisy@gmail.com>
2017-10-11 18:48:49 +0800

    Update Week6_2.java

--- Week6_2.java
+++ Week6_2.java
@@ -1,3 +1,102 @@
+//如下有两个版本
+//版本1：莫名AC自己写的（主要是利用indexOf(",")来定位判断）
+//版本2：网上copy的方法（主要是先利用split完成属性分割，再进行定位判断）
+
+import java.util.Scanner;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		String inputStr = new String(in.nextLine());
+		String lastStr = new String("");
+
+		while(inputStr.equals("END") == false )
+		{
+			String[] part = inputStr.split(",");
+			if(inputStr.startsWith("$GPRMC,"))
+			{
+				if(checkState(inputStr) && checkInt(inputStr)) 
+				{
+					lastStr = inputStr;
+				}
+			}
+			inputStr = in.nextLine();
+		}
+		if(lastStr == "") System.exit(0);
+		getTime(lastStr);
+	}
+
+	
+
+	public static void getTime(String inputStr)
+	{
+		int loc = 0;
+		loc = inputStr.indexOf(',');
+		int hour = 0, min = 0, sec = 0;
+		String hh = inputStr.substring(loc+1, loc+3);
+		String mm = inputStr.substring(loc+3, loc+5);
+		String ss = inputStr.substring(loc+5, loc+7);
+		hour = Integer.parseInt(hh);
+		hour = (hour + 8) % 24;
+		min = Integer.parseInt(inputStr.substring((loc+3), loc +5));
+		sec = Integer.parseInt(inputStr.substring((loc+5), loc+7));
+
+		System.out.print(String.format("%02d", hour));
+		System.out.print(":");
+		System.out.print(String.format("%02d", min));
+		System.out.print(":");
+		System.out.print(String.format("%02d", sec));
+	}
+
+	public static boolean checkState(String inputStr)
+	{
+		int loc = 0;
+		loc = inputStr.indexOf(',');
+		loc = inputStr.indexOf(',',loc+1);
+		if(inputStr.charAt(loc+1) == 'A' && inputStr.charAt(loc+2) == ',')
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public static boolean checkInt(String inputStr)
+	{
+		int i;
+		int testNum = 0;
+		char ch = inputStr.charAt(1);
+		testNum = ch;
+		for(i = 2; i < inputStr.length(); i++)
+		{
+			ch = inputStr.charAt(i);
+			if(ch == '*')
+			{
+				break;
+			}
+			testNum ^= ch;
+		}
+		testNum = (int)testNum % 65536;
+
+		String checkNum = inputStr.substring(inputStr.indexOf('*')+1);
+		checkNum = checkNum.toLowerCase();
+		if(checkNum.equals(Integer.toHexString(testNum)))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
+
+
+
 import java.util.Scanner;
 public class Main {
 	
